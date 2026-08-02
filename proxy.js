90e14b9feab8b7dc54a5e34e833c1d36552e9bbc
Parz1val-X <robertolaime61@gmail.com>
2026-08-01 22:55:45 -0400

    Fix: proxy interceptaba la imagen de login (imagenes) y redirigia a /login sin sesion

--- proxy.js
+++ proxy.js
@@ -28,5 +28,5 @@ export async function proxy(request) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next|imagenes|favicon.ico).*)'],
 };
